65397a943849b688610e2647c63a9c7ad4ad064a
mutharajuvamsikrishna <130433935+mutharajuvamsikrishna@users.noreply.github.com>
2024-05-09 17:16:46 +0530

    changes on config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react()
+  ],
+  // Custom build configuration
+  build: {
+    // Custom Rollup options
+    rollupOptions: {
+      // Custom onwarn function to handle warnings
+      onwarn: (warning, warn) => {
+        // Log the warning message to the console without aborting the build
+        console.warn(`[${warning.code}]: ${warning.message}`);
+        // Proceed with the build process
+        warn(warning);
+      }
+    }
+  }
+});
